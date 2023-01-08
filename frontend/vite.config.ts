6563430460b2491b1dcf1ef691cd7124003d382f
ib-R <business1232@gmail.com>
2023-01-08 19:35:02 +0200

    inject env vars in html

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
-export default defineConfig({
-  plugins: [solidPlugin()],
+export default defineConfig(({ mode }) => ({
+  plugins: [solidPlugin(), htmlPlugin(loadEnv(mode, '.')),],
   server: {
     port: 3000,
   },
   build: {
     target: 'esnext',
   },
-});
+}));
+
+function htmlPlugin(env: ReturnType<typeof loadEnv>) {
+  return {
+    name: 'html-transform',
+    transformIndexHtml: {
+      enforce: 'pre' as const,
+      transform: (html: string): string =>
+        html.replace(/%(.*?)%/g, (match, p1) =>
+          env[p1] ?? match
+        ),
+    }
+  }
+}
